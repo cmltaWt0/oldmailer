7c591a38bc89350ea2586fb83a6880cdf71b4a9a
Maksim Sokolski <misokolsky@gmail.com>
2014-03-05 14:36:39 +0200

    Add Exception to all with's block.

--- passwd_change.py
+++ passwd_change.py
@@ -2,7 +2,6 @@
 
 import sys
 
-
 _args = sys.argv
 
 if __name__ == "__main__":
@@ -11,17 +10,21 @@ if __name__ == "__main__":
         target_file = _args[2]
         result_file = _args[3]
 
-        with open(keys_file, 'r') as k:
-            keys = k.readlines()
-            keys = [key.strip() for key in keys]
-            keys = [key for key in keys if key != '']
-        with open(target_file, 'r') as t:
-            target_lines = t.readlines()
+        try:
+            with open(keys_file, 'r') as k:
+                keys = k.readlines()
+                keys = [key.strip().split('@')[0] for key in keys]
+                keys = [key for key in keys if key != '']
+            with open(target_file, 'r') as t:
+                target_lines = t.readlines()
 
-        with open(result_file, 'w') as r:
-            for line in target_lines:
-                if line.split(':')[0] in keys:
-                    r.write(line)
+            with open(result_file, 'w') as r:
+                for line in target_lines:
+                    if line.split(':')[0] in keys or line.split(':')[3] != '12':
+                        r.write(line)
+
+        except Exception as e:
+            print(str(e))
+            sys.exit()
     else:
         print('./passwd_change.py keys_file.txt passwd_file result_file')
-
