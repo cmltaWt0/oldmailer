ff50b3e43de0c083cd8c3daaa7644394daadc1a0
Maksim Sokolski <misokolsky@gmail.com>
2014-03-11 12:23:04 +0200

    Add tearDown() - remove test dir, test files existing and not existing.

--- test_passwd_change.py
+++ test_passwd_change.py
@@ -3,6 +3,7 @@
 from passwd_change import passwd_change, shadow_change, mails_delete
 
 from unittest import TestCase, TestLoader, TextTestRunner
+import os
 import subprocess
 
 
@@ -18,9 +19,24 @@ class PasswdChange_Test(TestCase):
         #TODO create shadow test file
         #TODO create keys.txt file
 
+    def tearDown(self):
+        if os.path.exists('test/rvv'):
+            raise Exception('test/rvv must not exist')
+        if not (os.path.exists('test/max') and
+                os.path.exists('test/bdv') and
+                os.path.exists('test/mail')):
+            raise Exception('File max, bdv or mail must exist!')
+        subprocess.call(['rm', '-r', 'test/'])
+
     def test_passwd_change(self):
         shadow_change(*passwd_change())
         mails_delete(maildir_path='test')
+        if os.path.exists('test/rvv'):
+            raise Exception('test/rvv must not exist')
+        if not (os.path.exists('test/max') and
+                os.path.exists('test/bdv') and
+                os.path.exists('test/mail')):
+            raise Exception('File max, bdv or mail must exist!')
 
     def test_passwd_change_2(self):
         shadow_change(*passwd_change())
